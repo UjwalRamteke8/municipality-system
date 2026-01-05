0d38fa7f9a9c1de706edc07a8b4702c34e329f8e
Ujwal <ujwalramteke293@gmail.com>
2026-01-05 22:58:54 +0530

    new changes for redeployment

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,10 +1,4 @@
-// backend/config/firebaseAdmin.js
-
 import admin from "firebase-admin";
-
-if (!process.env.FIREBASE_SERVICE_ACCOUNT_JSON) {
-  throw new Error("FIREBASE_SERVICE_ACCOUNT_JSON is not set");
-}
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
 
@@ -12,8 +6,6 @@ if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
-
-  console.log("✅ Firebase Admin initialized successfully");
 }
 
 export default admin;
--- frontend/src/api/apiClinet.js
+++ frontend/src/api/apiClinet.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAuth } from "firebase/auth";
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 apiClient.interceptors.request.use(
